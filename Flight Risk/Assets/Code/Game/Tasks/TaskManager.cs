d85c9491985ebfb99c3daeb94d3619224478398b
SuperGeniusCodingLegend <242620520+SuperGeniusCodingLegend@users.noreply.github.com>
2026-08-29 15:13:52 +0300

    changed passenger scripts to accomodate with new tasks

--- Flight Risk/Assets/Code/Game/Tasks/TaskManager.cs
+++ Flight Risk/Assets/Code/Game/Tasks/TaskManager.cs
@@ -7,27 +7,38 @@ namespace FlightRisk.Game
     /// <summary>
     /// haha
     /// </summary>
-    public class TaskManager : MonoBehaviour
+    public class TaskManager : MonoBehaviour , IRequireService<PassengerManager>
     {
         [SerializeField] private List<Task> regularTasks; // sprinkled across the game.
         [SerializeField] private List<Task> specialTasks; // at least one per a time threshold of our choosing.
 
+        private PassengerManager passengerManager;
+
         private HashSet<Task> runningTasks = new();
         private HashSet<Task> tasksEndedThisFrame = new();
 
-        private void Update()
+        private bool canSpawnTasks;
+
+        private void Awake()
         {
-            RunTasks();
+            this.WaitForService(pissman => passengerManager = pissman);
         }
 
-        private void RunTasks()
+        private void Update()
         {
+            RunCurrentTasks();
+        }
+
+        private void RunCurrentTasks()
+        {
+            if (runningTasks.Count == 0) return;
+
             foreach (var task in runningTasks)
             {
                 var state = task.TaskTick();
                 if (state == Task.State.Active) continue;
 
-                if (state == Task.State.Complete) 
+                if (state == Task.State.Complete)
                     GameStatus.PassengerSatisfaction += Mathf.Abs(task.SatisfactionGainOnComplete);
                 else 
                     GameStatus.PassengerSatisfaction -= Mathf.Abs(task.SatisfactionLossOnFail);
@@ -37,6 +48,16 @@ namespace FlightRisk.Game
 
             runningTasks.RemoveWhere(task => tasksEndedThisFrame.Contains(task));
             tasksEndedThisFrame.Clear();
+        }
+
+        private void SpawnPeriodicTasks()
+        {
+
+        }
+
+        private Task CreateTask()
+        {
+
         }
     }
 }
